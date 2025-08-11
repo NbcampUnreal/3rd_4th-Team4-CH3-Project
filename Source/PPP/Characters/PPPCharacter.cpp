067cec7b48b43ef0a35afa3239c2ae665e6baf10
Hyunsu Jung <jhs880514@naver.com>
2025-08-11 12:35:37 +0900

    캐릭터 애니메이션 블루프린트 추가 및 돌격소총 장착 기능 구현
    Signed-off-by: Hyunsu Jung <jhs880514@naver.com>

--- Source/PPP/Characters/PPPCharacter.cpp
+++ Source/PPP/Characters/PPPCharacter.cpp
@@ -68,6 +68,9 @@ APppCharacter::APppCharacter()
     EquippedWeapon = nullptr;
 
     bIsReloading = false;
+
+    // 총 없는 상태에서 시작
+    //bIsRifleEquipped = false;
 }
 
 void APppCharacter::BeginPlay()
@@ -500,62 +503,3 @@ void APppCharacter::OnWeaponAmmoChanged(int32 CurrentAmmoInMag, int32 ReserveAmm
     OnAmmoChanged.Broadcast(CurrentAmmoInMag, ReserveAmmo);
     UE_LOG(LogTemp, Warning, TEXT("탄약 변경: 현재 탄창 %d, 예비 탄약 %d"), CurrentAmmoInMag, ReserveAmmo);
 }
-
-// 성준모, 장전 입력 시 호출되는 함수 구현
-void APppCharacter::OnReload()
-{
-    UE_LOG(LogTemp, Warning, TEXT("[Check] OnReload pressed"));
-
-    // 장착된 무기가 없을 때 실행
-    if (!EquippedWeapon)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Reload 실패 : 장착된 무기가 없습니다."));
-        return;
-    }
-
-    // 재장전 중복 방지
-    if (bIsReloading)
-    {
-        // Verbose : 상세 로그 레벨로, 매우 세부적인 진단용 메세지.
-        UE_LOG(LogTemp, Verbose, TEXT("이미 재장전 중입니다."));
-        return;
-    }
-
-    // 데이터 테이블에서 온 ReloadTime 사용
-    float ReloadTime = EquippedWeapon->WeaponDataRow.ReloadTime;
-
-    bIsReloading = true;  // 핵심 함수, 무기 재장전 (무기 Reload 호출)
-
-    // 유효한 장전 시간이면 타이머, 아니면 즉시 완료
-    if (ReloadTime > 0.f)
-    {
-        GetWorldTimerManager().SetTimer(ReloadTimerHandle, this, &APppCharacter::FinishReload, ReloadTime, false);
-    }
-    else
-    {
-        // 장전 시간이 0이거나 잘못된 경우 즉시 완료 처리
-        FinishReload();
-    }
-}
-
-// 성준모, 타이머 완료 시 호출되어 실제 탄약을 채우는 함수
-void APppCharacter::FinishReload()
-{
-    // 타이머 초기화
-    GetWorldTimerManager().ClearTimer(ReloadTimerHandle);
-
-    // 장착된 무기가 없을 때 실행
-    if (!EquippedWeapon)
-    {
-        bIsReloading = false;
-        UE_LOG(LogTemp, Warning, TEXT("재장전 실패: 무기가 해제되었습니다."));
-        return;
-    }
-
-    // 실제 탄창 채우기, OnAmmoChanged 브로드 캐스트 까지 수행
-    EquippedWeapon->Reload();
-
-    bIsReloading = false;
-    UE_LOG(LogTemp, Log, TEXT("재장전 완료"));
-}
-
